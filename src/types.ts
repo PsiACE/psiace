cbd78529ee41c7988a04679a26e76e1c0397a584
Chojan Shang <psiace@apache.org>
2024-08-18 19:58:21 +0800

    feat: add giscus
    Signed-off-by: Chojan Shang <psiace@apache.org>

--- src/types.ts
+++ src/types.ts
@@ -8,6 +8,9 @@ export type SiteConfig = {
 		locale: string | string[] | undefined;
 		options: Intl.DateTimeFormatOptions;
 	};
+	comments: {
+		giscus: object;
+	};
 };
 
 export type PaginationLink = {
